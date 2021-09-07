a811934c1aa84bfce879e0bdaf7ac23efcb0b692
Ming Wang <mingw4@illinois.edulayoulayoutlllww>
2021-09-06 19:12:40 -0500

    NON_DONE

--- extreme_edge_cases/camelCaser.c
+++ extreme_edge_cases/camelCaser.c
@@ -15,18 +15,18 @@ char **camel_caser(const char *input_str) {
     int sntns_num = 0;
     int chars_num = 0;
     int curr_sntn_num = 0;
-    int alpha_pos = 0
+    int alpha_pos = 0;
 
-    for (int i = 0; i < strlen(input_str); i++) {
-        if (ispunct(input_str[i]))) {
+    for (unsigned long i = 0; i < strlen(input_str); i++) {
+        if (ispunct(input_str[i])) {
             sntns_num++;
         }
     }
 
-    char** sntns = (char**)malloc(sizeof(char*) * (num_sen + 1));
-    rslt[sen_num] = NULL;
+    char** sntns = (char**)malloc(sizeof(char*) * (sntns_num + 1));
+    sntns[sntns_num] = NULL;
 
-    for (int j = 0; j < strlen(input_str); j++) {
+    for (unsigned long j = 0; j < strlen(input_str); j++) {
         if (ispunct(input_str[j])) {
             sntns[curr_sntn_num] = malloc(chars_num + 1);
             memcpy(sntns[curr_sntn_num], input_str + alpha_pos, chars_num);
@@ -56,8 +56,8 @@ char **camel_caser(const char *input_str) {
                 parse = 1;
             
                 if (curr_pos) {
-                    to_word(sntns[k][l] + initial_pos, curr_pos, ptr);
-                    rslts[k] = curr_pos + initial_pos;
+                    to_word(sntns[k] + initial_pos, curr_pos, ptr);
+                    rslts[k] += curr_pos;
                 }
 
                 curr_pos = 0;
@@ -65,14 +65,14 @@ char **camel_caser(const char *input_str) {
                 if (parse) {
                     curr_pos = 1;
                     initial_pos = l;
-                    parse = 0
+                    parse = 0;
                 } else {
                     curr_pos = curr_pos + 1;
                 }
             }
         }
         if (curr_pos) {
-            to_word(sntns[k] + initial_pos, curr_pos, ptr);
+            to_word((const char *)(sntns[k] + initial_pos), curr_pos, ptr);
             ptr = ptr + curr_pos;
             curr_pos = 0;
         }
@@ -85,7 +85,26 @@ char **camel_caser(const char *input_str) {
     }
     destroy(sntns);
 
-    return 
+    return rslts;
+}
+
+void to_word(const char *w, int lnth, char *rt) {
+    int start = 1;
+    for (int i = 0; i < lnth; i++) {
+        if (isalpha(w[i])) {
+            if (start) {
+                *rt = w[i] >= 'a' ? w[i] - 32 : w[i];
+            } else {
+                *rt = w[i] >= 'a' ? w[i] : w[i] + 32;
+            }
+        } else {
+            *rt = w[i];
+        }
+        if (start) {
+            start = 0;
+        }
+        rt++;
+    }
 }
 
 void destroy(char **result) {
--- extreme_edge_cases/camelCaser_tests.c
+++ extreme_edge_cases/camelCaser_tests.c
@@ -12,5 +12,130 @@
 int test_camelCaser(char **(*camelCaser)(const char *),
                     void (*destroy)(char **)) {
     // TODO: Implement me!
+    char **rt = 0;
+
+
+    rt = (*camelCaser)("");
+    if (rt[0] != 0) {
+        destroy(rt);
+        return 0;
+    }
+    destroy(rt);
+
+    rt = (*camelCaser)("thisis madddddddddddddddd") 
+    if (rt[0] != NULL) {
+        destroy(rt);
+        return 0;
+    }
+    destroy(rt);
+
+
+
+    rt = (*camelCaser)("/\,.>?");
+    if (rt[0] != "") {
+        destroy(rt);
+        return 0;
+    }
+    if (rt[1] != "") {
+        destroy(rt);
+        return 0;
+    }
+    if (rt[2] != "") {
+        destroy(rt);
+        return 0;
+    }
+
+    if (rt[3] != "") {
+        destroy(rt);
+        return 0;
+    }
+
+    if (rt[4] != "") {
+        destroy(rt);
+        return 0;
+    }
+
+    if (rt[5] != "") {
+        destroy(rt);
+        return 0;
+    }
+
+    if (rt[6] != NULL) {
+        destroy(rt);
+        return 0;
+    }
+    destroy(rt);
+
+
+    rt = (*camelCaser)("CS241 is Literally mad.");
+    if (rt[0] != "cS241IsLiteralyMad") {
+        destroy(rt);
+        return 0;
+    }
+    if (rt[1] != NULL) {
+        return 0;
+    }
+    destroy(rt);
+
+    rt = (*camelCaser)("WWw. WHWHWH nmmi. TREfefdds.");
+    if (rt[0] != "wwww") {
+        destroy(rt);
+        return 0;
+    }
+    if (rt[1] != "whwhwhNmmi") {
+        destroy(rt);
+        return 0;
+    }
+    if (rt[2] != "trefefdds") {
+        destroy(rt);
+        return 0;
+    }
+
+    if (rt[3] != NULL) {
+        destroy(rt);
+        return 0;
+    }
+    destroy(rt);
+
+
+    rt = (*camelCaser)("241Nonsense.");
+    if (rt[0] != "241nonsense") {
+        destroy(rt);
+        return 0;
+    }
+    if (rt[1] != NULL) {
+        destroy(rt);
+        return 0;
+    }
+    destroy(rt);
+
+    rt = (*camelCaser)("FFFFF.  WHATAahtata is this  \n for?")
+    if (rt[0] != "fffff") {
+        destroy(rt);
+        return 0;
+    }
+
+    if (rt[1] != "whatahtataIsThisFor") {
+        destroy(rt);
+        return 0;
+    }
+
+    if (rt[2] != NULL) {
+        destroy(rt);
+        return 0;
+    }
+
+    destroy(rt);
+
+    rt = (*camelCaser)("变态中文测试.   Chinese test."); 
+    if (rt[0] != "变态中文测试") {
+        destroy(rt);
+        return 0;
+    }
+
+
+
+
+    
     return 1;
 }
