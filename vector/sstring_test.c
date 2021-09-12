ca41cd01c9182d04b42a23fa9093a5bc6487ae4d
Ming Wang <mingw4@illinois.edulayoulayoutlllww>
2021-09-12 17:29:41 -0500

    MP_Done?

--- vector/sstring_test.c
+++ vector/sstring_test.c
@@ -3,8 +3,14 @@
  * CS 241 - Fall 2021
  */
 #include "sstring.h"
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
     // TODO create some tests
+    sstring* test = cstr_to_sstring("abcd");
+    char* sliced = sstring_slice(test, 1, 3);
+    printf("sliced is: %s\n", sliced);
+    free(sliced);
+    sstring_destroy(test);
     return 0;
 }
