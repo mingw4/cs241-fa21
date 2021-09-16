59345fd60f585e1071fb2a5cc9459d48e76d36f7
Ming Wang <mingw4@illinois.edulayoulayoutlllww>
2021-09-15 22:12:41 -0500

    done

--- utilities_unleashed/env.c
+++ utilities_unleashed/env.c
@@ -2,6 +2,8 @@
  * utilities_unleashed
  * CS 241 - Fall 2021
  */
+
+
 #include "format.h"
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -12,34 +14,39 @@
 #include <ctype.h>
 
 int main(int argc, char *argv[]) {
-
     if (argc < 4) {
-        print_env_usage();
+    print_env_usage();
     }
+
     pid_t p = fork();
     if (p == 0) {
-        for (unsigned int n = 1; argv[n]; n++) {
+        for (int n = 1; argv[n]; n++) {
             if (strcmp("--", argv[n])) {
-                char* v = strtok(NULL, "");
-                char* k = strtok(argv[n], "=");
-                if (!v) {
+
+                char *k = strtok(argv[n], "=");
+                char *v = strtok(NULL, "");
+                if (v == NULL) {
                     print_env_usage();
                 }
-                for (char* itr = k; (*itr); itr++) {
-                    if (!(isalpha(*itr) || isdigit(*itr) || *itr == '_')) {
+                char *itr = k;
+                while (*itr) {
+                    if (*itr != '_' && (!isdigit(*itr)) && (!isalpha(*itr))) {
                         print_env_usage();
                     }
+                    itr++;
                 }
-                if (v[0] != '%') {
-                    for (char* itr = v; (*itr); itr++) {
-                        if (!(isalpha(*itr) || isdigit(*itr) || *itr == '_')) {
-                            print_environment_change_failed();
-                        }
-                    }
-                } else {
+                if (v[0] == '%') {
                     v = getenv(v + 1);
                     if (!v) {
                         print_environment_change_failed();
+                    }
+                } else {
+                    char *itr = v;
+                    while (*itr) {
+                        if (*itr != '_' && (!isdigit(*itr)) && (!isalpha(*itr))) {
+                            print_env_usage();
+                        }
+                        itr++;
                     }
                 }
                 if (setenv(k, v, 1) < 0) {
@@ -51,14 +58,11 @@ int main(int argc, char *argv[]) {
             }
         }
         print_env_usage();
-    } else if (p > 0) {
+    } else if (p < 0) {
+        print_fork_failed();
+    } else {
         int flag_;
         waitpid(p, &flag_, 0);
-        return 0;
-    } else {
-        print_fork_failed();
     }
-
-
     return 0;
 }
