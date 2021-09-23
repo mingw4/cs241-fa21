72101f586c47d5f20a09bddd21592a846532953a
Ming Wang <mingw4@illinois.edulayoulayoutlllww>
2021-09-22 23:50:53 -0500

    test++

--- mini_memcheck/test.c
+++ mini_memcheck/test.c
@@ -7,5 +7,10 @@
 
 int main(int argc, char *argv[]) {
     // Your tests here using malloc and free
+    void *p1 = malloc(30);
+    void *p2 = malloc(40);
+    void *p3 = malloc(50);
+    free(p2);
+    return 0;
     return 0;
 }
