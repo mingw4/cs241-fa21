4f6e98800a2a0dfcbe0dd4fb41ac094650bfa8e8
Ming Wang <mingw4@illinois.edulayoulayoutlllww>
2021-09-27 22:06:59 -0500

    fourth

--- shell/shell.c
+++ shell/shell.c
@@ -12,26 +12,35 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/sysinfo.h>
 #include <ctype.h>
 #include <getopt.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "time.h"
 
 void sgn_(int sig);
 int Fg (char * cmd);
 void destroy_();
 void Open_source(int argc, char *argv[]);
 void Exit_source();
+char *copy_string(const char *s);
 
 typedef struct process {
     char *command;
     pid_t pid;
 } process;
 
+void print_proc_info(pid_t pid, char *command);
+long get_uptime();
+
 static vector *Present;
 static vector *hst = NULL;
 static char *hst_in = NULL;
 static FILE *source = NULL;
-
+static long uptime = 0;
+static long ticks_per_second;
 
 
 void sgn_(int sig) {
@@ -120,68 +129,119 @@ int Fg(char *cmd) {
         } else {
             return 0;
         }
-    } else {
-        fflush(stdout);
-        int bg = 0;
-        if (cmd[strlen(cmd) - 1] == '&') {
-            bg = 1;
-        }
-        pid_t f_ = fork();
-        if (f_ < 0) {
-            print_fork_failed();
-            exit(1);
-        }
-        if (f_ == 0) {
-            if (bg == 1) {
-                cmd[strlen (cmd) - 1] = '\0';
-            }
-            vector *p = sstring_split(cstr_to_sstring(cmd), ' ');
-            char *tet[vector_size(p)+1];
-            for (size_t i = 0; i < vector_size(p); i++) {
-                tet[i] = (char *) vector_get(p, i);
-            }
-            if (!strcmp(tet[vector_size(p)-1], ""))
-                tet[vector_size(p)-1] = NULL;
-            else
-                tet[vector_size(p)] = NULL;
-            print_command_executed(getpid());
-            execvp(tet[0], tet);
-            print_exec_failed(tet[0]);
-            exit(1);
-        }
-        if (f_ > 0) {
-            process *tmp = calloc(1, sizeof(Present));
-            tmp->command = calloc(strlen(cmd) + 1, sizeof(char));
-            strcpy(tmp->command, cmd);
-            tmp->pid = f_;
+    }
+    fflush(stdout);
+    int bg = 0;
+    if (cmd[strlen(cmd) - 1] == '&') {
+        bg = 1;
+    }
 
-            vector_push_back(Present, tmp);
-            if (bg == 1) {
-                if (setpgid(f_, f_) == -1) {
-                    print_setpgid_failed();
-                    exit(1);
+    pid_t f_ = fork();
+    if (f_ < 0) {
+        print_fork_failed();
+        exit(1);
+    }
+    if (f_ == 0) {
+        if (bg == 1) {
+            cmd[strlen (cmd) - 1] = '\0';
+        }
+        char *input_file = NULL;
+        char *output_file = NULL;
+        int append_output = 0;
+        vector *p = sstring_split(cstr_to_sstring(cmd), ' ');
+        size_t len = vector_size(p);
+        for (size_t j = 0; j < len; ) {
+            if (strcmp((char *)vector_get(p, j), "<") == 0 && j + 1 < len) {
+                input_file = copy_string((char *)vector_get(p, j + 1));
+                vector_set(p, j, NULL);
+                vector_set(p, j + 1, NULL);
+                j += 2;
+            } else if (strcmp((char *)vector_get(p, j), ">") == 0 && j + 1 < len) {
+                output_file = copy_string((char *)vector_get(p, j + 1));
+                vector_set(p, j, NULL);
+                vector_set(p, j + 1, NULL);
+                j += 2;
+            } else if (strcmp((char *)vector_get(p, j), ">>") == 0 && j + 1 < len) {
+                output_file = copy_string((char *)vector_get(p, j + 1));
+                append_output = 1;
+                vector_set(p, j, NULL);
+                vector_set(p, j + 1, NULL);
+                j += 2;
+            } else {
+                ++j;
+            }
+        }
+
+        char *tet[vector_size(p)+1];
+        int n_args = 0;
+        for (size_t i = 0; i < vector_size(p); i++) {
+            char *arg = (char *) vector_get(p, i);
+            if (arg != NULL)
+                tet[n_args++] = (char *) arg;
+        }
+        if (!strcmp(tet[n_args-1], ""))
+            tet[n_args-1] = NULL;
+        else
+            tet[n_args] = NULL;
+        print_command_executed(getpid());
+
+        if (input_file != NULL) {
+            int fd = open(input_file, O_RDONLY);
+            if (fd != -1) {
+                dup2(fd, STDIN_FILENO);
+                close(fd);
+            }
+            free(input_file);
+        }
+
+        if (output_file != NULL) {
+            int flags = O_WRONLY | O_CREAT;
+
+            if (append_output)
+                flags |= O_APPEND;
+            else
+                flags |= O_TRUNC;
+            int fd = open(output_file, flags, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+            if (fd != -1) {
+                dup2(fd, STDOUT_FILENO);
+                close(fd);
+            }
+            free(output_file);
+        }
+        execvp(tet[0], tet);
+        print_exec_failed(tet[0]);
+        exit(1);
+    }
+    if (f_ > 0) {
+        process *tmp = calloc(1, sizeof(Present));
+        tmp->command = calloc(strlen(cmd) + 1, sizeof(char));
+        strcpy(tmp->command, cmd);
+        tmp->pid = f_;
+
+        vector_push_back(Present, tmp);
+        if (bg == 1) {
+            if (setpgid(f_, f_) == -1) {
+                print_setpgid_failed();
+                exit(1);
+            }
+        } else {
+            if (setpgid(f_, getpid()) == -1) {
+                print_setpgid_failed();
+                exit(1);
+            }
+            int status;
+            int error = waitpid(f_, &status, 0);
+            if (error != -1) {
+                if (WIFEXITED(status) && WEXITSTATUS(status)) {
+                    return 1;
                 }
             } else {
-                if (setpgid(f_, getpid()) == -1) {
-                    print_setpgid_failed();
-                    exit(1);
-                }
-                int status;
-                int error = waitpid(f_, &status, 0);
-                if (error != -1) {
-                    if (WIFEXITED(status) && WEXITSTATUS(status)) {
-                        return 1;
-                    }
-                } else {
-                    print_wait_failed();
-                    exit(1);
-                }
+                print_wait_failed();
+                exit(1);
             }
         }
-
-
-
     }
+
     return 0;
 }
 
@@ -191,6 +251,9 @@ int shell(int argc, char *argv[]) {
     signal(SIGINT, sgn_);
     signal(SIGCHLD, sgn_);
 
+
+    ticks_per_second = sysconf(_SC_CLK_TCK);
+    uptime = get_uptime();
 
     source = stdin;
     hst = string_vector_create();
@@ -258,6 +321,14 @@ int shell(int argc, char *argv[]) {
         } else if (!strcmp(in,"exit")) {
             destroy_(); 
             break;
+        } else if (!strcmp(in, "ps")) {
+            size_t len = vector_size(Present);
+            print_process_info_header();
+            for (size_t j = 0; j < len; ++j) {
+                process *p = vector_get(Present, j);
+                print_proc_info(p->pid, p->command);
+            }
+            print_proc_info(getpid(), argv[0]);
         } else {
             vector_push_back(hst, in);
             int flag_ = 0;
@@ -307,4 +378,83 @@ int shell(int argc, char *argv[]) {
     Exit_source();
 
     return 0;
+}
+
+void print_proc_info(pid_t pid, char *command) {
+    char filename[64];
+    sprintf(filename, "/proc/%d/stat", pid);
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL)
+        return;
+
+    char buffer[1024];
+    if (fread(buffer, 1, sizeof(buffer), fp) <= 0) {
+        fclose(fp);
+        return;
+    }
+
+    process_info info;
+    info.pid = pid;
+    info.command = command;
+
+    char *token = strtok(buffer, " ");
+    time_t start_time;
+    size_t cpu_time = 0;
+
+    for (int i = 1; i <= 23 && token != NULL; ++i) {
+        switch (i) {
+        case 3:
+            info.state = token[0];
+            break;
+        case 14:
+        case 15:
+            {
+                unsigned long t;
+                sscanf(token, "%lu", &t);
+                cpu_time += t / ticks_per_second;
+            }
+            break;
+        case 20:
+            sscanf(token, "%ld", &info.nthreads);
+            break;
+        case 22:
+            {
+                unsigned long long t;
+                sscanf(token, "%llu", &t);
+                start_time = (time_t)(t / ticks_per_second) + (time_t)uptime;
+            }
+            break;
+        case 23:
+            sscanf(token, "%lu", &info.vsize);
+            info.vsize /= 1024;
+            break;
+        default:
+            break;
+        }
+        token = strtok(NULL, " ");
+    }
+
+    info.start_str = buffer;
+    info.time_str = buffer + 65;
+
+    execution_time_to_string(info.time_str, 64, cpu_time / 60, cpu_time % 60);
+    time_struct_to_string(info.start_str, 64, localtime(&start_time));
+
+    print_process_info(&info);
+
+    fclose(fp);
+}
+
+long get_uptime() {
+    long res;
+    struct sysinfo info;
+    sysinfo(&info);
+    res = time(NULL) - info.uptime;
+    return res;
+}
+
+char *copy_string(const char *s) {
+    char *new_s = malloc(strlen(s) + 1);
+    strcpy(new_s, s);
+    return new_s;
 }
