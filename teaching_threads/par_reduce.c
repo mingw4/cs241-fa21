5648ee48f658749404316356e321d8c39d8620c5
Ming Wang <mingw4@illinois.edulayoulayoutlllww>
2021-09-29 21:00:12 -0500

    done?

--- teaching_threads/par_reduce.c
+++ teaching_threads/par_reduce.c
@@ -21,11 +21,12 @@ typedef struct structure_t {
 } structure_t;
 
 /* You should create a start routine for your threads. */
-void* start_routine(void* par) {
-    structure_t *buffer = (structure_t*) par;
-    int *res = malloc(sizeof(int));
-    *res = reduce(buffer->list, buffer->list_len, buffer->reduce_func, buffer->base_case);
-    return (void*) res;
+void* start_routine(void* p) {
+    structure_t* rt = (structure_t*) p;
+    for (size_t j = 0; j < rt->list_len; j++) {
+        rt->base_case = rt->reduce_func(rt->base_case, rt->list[j]);
+    }
+    pthread_exit(NULL);
 }
 
 int par_reduce(int *list, size_t list_len, reducer reduce_func, int base_case,
@@ -34,33 +35,28 @@ int par_reduce(int *list, size_t list_len, reducer reduce_func, int base_case,
     if (num_threads >= list_len) {
         return reduce(list, list_len, reduce_func, base_case);
     }
-    int lists[num_threads];
     pthread_t pthreads[num_threads];
     int thread_size = list_len / num_threads;
-    structure_t* par[num_threads];
+    structure_t* par = calloc(num_threads, sizeof(structure_t));
     for (size_t j = 0; j < num_threads - 1; j++) {
-        par[j] = malloc(sizeof(structure_t));
-        par[j]->list = list + j * thread_size;
-        par[j]->list_len = thread_size;
-        par[j]->reduce_func = reduce_func;
-        par[j]->base_case = base_case;
+        par[j].list = list + j * thread_size;
+        par[j].list_len = thread_size;
+        par[j].reduce_func = reduce_func;
+        par[j].base_case = base_case;
+        pthread_create(pthreads + j, NULL, start_routine, par + j);
     }
-    par[num_threads] = malloc(sizeof(structure_t));
-    par[num_threads]->list = list + num_threads * thread_size;
-    par[num_threads]->list_len = list_len - (thread_size * num_threads);
-    par[num_threads]->reduce_func = reduce_func;
-    par[num_threads]->base_case = base_case;
-
+    par[num_threads - 1].list = list + ((num_threads - 1) * thread_size);
+    par[num_threads - 1].list_len = list_len - ((num_threads - 1) * thread_size);
+    par[num_threads - 1].reduce_func = reduce_func;
+    par[num_threads - 1].base_case = base_case;
+    pthread_create(pthreads + (num_threads - 1), NULL, start_routine, par + (num_threads - 1));
     for (size_t k = 0; k < num_threads; k++) {
-        pthread_create(&pthreads[k], 0, start_routine, (void*)par[k]);
+        pthread_join(pthreads[k], NULL);
     }
+    int result = base_case;
     for (size_t l = 0; l < num_threads; l++) {
-        void *buffer;
-        pthread_join(pthreads[l],&buffer);
-        lists[l] = *((int*)buffer);
-        free(buffer);
-        free(par[l]);
+        result = reduce_func(result, par[l].base_case);
     }
-    int result = reduce(lists, num_threads, reduce_func, base_case);
     return result;
+ 
 }
