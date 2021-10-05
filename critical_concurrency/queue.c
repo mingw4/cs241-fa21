c8abf4dcc41bb9c4b0dde048574b2d182c9512af
Ming Wang <mingw4@illinois.edulayoulayoutlllww>
2021-10-05 18:48:22 -0500

    queue done

--- critical_concurrency/queue.c
+++ critical_concurrency/queue.c
@@ -36,18 +36,77 @@ struct queue {
 
 queue *queue_create(ssize_t max_size) {
     /* Your code here */
-    return NULL;
+    queue *result = malloc(sizeof(queue));
+    if (result == NULL) {
+        return NULL;
+    }
+    result->head = NULL;
+    result->tail = NULL;
+    result->size = 0;
+    result->max_size = max_size;
+    pthread_cond_destroy(&(result->cv));
+    pthread_mutex_destroy(&(result->m));
+    return result;
 }
 
 void queue_destroy(queue *this) {
     /* Your code here */
+    if (!this) {
+        return;
+    }
+    queue_node* curr = this->head;
+    while(curr) {
+        queue_node* buffer = curr;
+        curr = curr->next;
+        free(buffer);
+    }
+    pthread_cond_destroy(&(this->cv));
+    pthread_mutex_destroy(&(this->m));
+    free(this);
 }
 
 void queue_push(queue *this, void *data) {
     /* Your code here */
+    pthread_mutex_lock(&(this->m));
+    while (this->max_size > 0 && this->size >= this->max_size) {
+        pthread_cond_wait(&(this->cv), &(this->m));
+    }
+    queue_node* result = malloc(sizeof(queue_node));
+    result->data = data;
+    result->next = NULL;
+    if (this->size == 0) {
+        this->head = result;
+        this->tail = result;
+        this->size = this->size + 1;
+        pthread_cond_broadcast(&(this->cv));
+        pthread_mutex_unlock(&(this->m));
+    } else {
+        this->tail->next = result;
+        this->tail = result;
+        this->size = this->size + 1;
+        pthread_cond_broadcast(&(this->cv));
+        pthread_mutex_unlock(&(this->m));
+    }
 }
 
 void *queue_pull(queue *this) {
     /* Your code here */
-    return NULL;
+    pthread_mutex_lock(&(this->m));
+    while (this->head == NULL) {
+        pthread_cond_wait(&(this->cv), &(this->m));
+    }
+    queue_node *buffer = this->head;
+    if (this->size == 1) {
+        this->head = NULL;
+        this->tail = NULL;
+    } else {
+        this->head = buffer->next;
+    }
+    void* result = buffer->data;
+    this->size = this->size - 1;
+    free(buffer);
+    pthread_cond_broadcast(&(this->cv));
+    pthread_mutex_unlock(&(this->m));
+
+    return result;
 }
