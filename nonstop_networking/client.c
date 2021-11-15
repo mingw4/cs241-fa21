c29882835fc8ddfc613b7bb0137f346b794b0559
mingw4 <mingw4@illinois.edu>
2021-11-15 09:08:31 -0600

    second

--- nonstop_networking/client.c
+++ nonstop_networking/client.c
@@ -11,8 +11,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netdb.h>
 #include <errno.h>
+#include <fcntl.h>
 
 #include "common.h"
 
@@ -41,6 +43,12 @@ int connect_to_server(const char *host, const char *port) {
     return fd;
 }
 
+bool has_more_byte(int fd) {
+    char byte;
+    return read_all(fd, &byte, 1) == 0;
+}
+
+// Send the request header to the server.
 int send_request_header(int fd, verb command, const char *filename) {
     char buff[MAX_VERB_LEN + 1 + MAX_FILENAME_LEN + 2];
     switch (command)
@@ -72,6 +80,7 @@ int send_request_header(int fd, verb command, const char *filename) {
     return 0;
 }
 
+// Read the response code and the error message, if any.
 int read_response_code(int fd, char **p_error) {
     char buff[7];
     if (read_all(fd, buff, 1) == -1) {
@@ -83,7 +92,7 @@ int read_response_code(int fd, char **p_error) {
         if (read_all(fd, buff + 1, 2) == -1 || strcmp(buff, "OK\n") != 0) {
             return -1;
         }
-        LOG("STATUS_ERROR");
+        LOG("STATUS_OK");
     } else {
         buff[6] = '\0';
         if (read_all(fd, buff + 1, 5) == -1 || strcmp(buff, "ERROR\n") != 0) {
@@ -118,12 +127,50 @@ int read_response_code(int fd, char **p_error) {
     return 0;
 }
 
-int handle_get(int sock_fd, char **args) {
-    if (send_request_header(sock_fd, GET, args[3]) == -1) {
-        return -1;
+// Copy the content of a file from one fd to another.
+size_t copy_file(int src_fd, int dst_fd, size_t size) {
+    char buff[256];
+    if (size == 0) {
+        if (has_more_byte(src_fd)) {
+            return 1;
+        }
+        return 0;
     }
 
-    char buff[256];
+    size_t write_len = 0;
+
+    while (write_len < size) {
+        // Read a chunk of data from the input file.
+        ssize_t len = read(src_fd, buff, sizeof(buff));
+        if (len > (ssize_t) (size - write_len)) {
+            print_received_too_much_data();
+            write_len += len;
+            break;
+        }
+
+        if (len == 0 || (len == -1 && errno != EINTR)) {
+            // EOF or error.
+            break;
+        }
+
+        if (len > 0) {
+            // Write the chunk of data to the output file.
+            if (write_all(dst_fd, buff, len) == -1) {
+                break;
+            }
+            write_len += len;
+        }
+    }
+    return write_len;
+}
+
+void handle_get(int sock_fd, char **args) {
+    // Send the header.
+    if (send_request_header(sock_fd, GET, args[3]) == -1) {
+        return;
+    }
+
+    shutdown(sock_fd, SHUT_WR);
 
     LOG("processing response");
 
@@ -131,61 +178,139 @@ int handle_get(int sock_fd, char **args) {
     char *err = NULL;
     if (read_response_code(sock_fd, &err) == -1) {
         print_invalid_response();
-        return -1;
+        return;
     }
     if (err != NULL) {
         print_error_message(err);
         free(err);
-        return -1;
+        return;
     }
-
 
     // Read the size of the file.
     size_t file_size;
     if (read_size(sock_fd, &file_size) == -1) {
         print_invalid_response();
-        return -1;
+        return;
     }
 
-    FILE *out_fp = fopen(args[4], "w");
-    if (out_fp) {
+    int out_fd = open(args[4], O_CREAT | O_TRUNC | O_WRONLY, 0777);
+    if (out_fd == -1) {
         perror(args[4]);
-        return -1;
+        return;
     }
 
-    int return_code = 0;
+    size_t received_size = copy_file(sock_fd, out_fd, file_size);
+    if (received_size > file_size) {
+        print_received_too_much_data();
+    } else if (received_size < file_size) {
+        print_too_little_data();
+    }
+    close(out_fd);
+}
 
-    while (file_size > 0) {
-        ssize_t len = read(sock_fd, buff, sizeof(buff));
-        if (len > (ssize_t) file_size) {
-            print_received_too_much_data();
-            return_code = -1;
-            break;
-        }
-
-        if (len == -1 && errno != EINTR) {
-            print_too_little_data();
-            return_code = -1;
-            break;
-        }
-        fwrite(buff, 1, len, out_fp);
-        file_size -= len;
+void handle_put(int sock_fd, char **args) {
+    if (send_request_header(sock_fd, PUT, args[3]) == -1) {
+        return;
     }
 
-    fclose(out_fp);
-    return return_code;
+    // Open the file to send
+    int in_fd = open(args[4], O_RDONLY);
+    if (in_fd == -1) {
+        perror("open");
+        return;
+    }
+
+    // Get the file size using fstat
+    struct stat stat;
+    if (fstat(in_fd, &stat) == -1) {
+        perror("fstat");
+        close(in_fd);
+        return;
+    }
+
+    // Send the file size
+    const size_t file_size = stat.st_size;
+    if (write_all(sock_fd, &file_size, sizeof(file_size)) == -1) {
+        perror("write");
+        close(in_fd);
+        return;
+    }
+
+    // Send the file content
+    copy_file(in_fd, sock_fd, file_size);
+    close(in_fd);
+    shutdown(sock_fd, SHUT_WR);
+
+    // Read the response
+    char *err = NULL;
+    if (read_response_code(sock_fd, &err) == -1) {
+        print_invalid_response();
+        return;
+    }
+    if (err != NULL) {
+        print_error_message(err);
+        free(err);
+        return;
+    }
+
+    print_success();
 }
 
-int handle_put(int sock_fd, char **args) {
-    return 0;
+void handle_delete(int sock_fd, char **args) {
+    if (send_request_header(sock_fd, DELETE, args[3]) == -1) {
+        return;
+    }
+
+    shutdown(sock_fd, SHUT_WR);
+
+    // Read the response
+    char *err = NULL;
+    if (read_response_code(sock_fd, &err) == -1) {
+        print_invalid_response();
+        return;
+    }
+    if (err != NULL) {
+        print_error_message(err);
+        free(err);
+        return;
+    }
+
+    print_success();
 }
 
-int handle_delete(int sock_fd, char **args) {
-    return 0;
-}
+// Handle the LIST command
+void handle_list(int sock_fd, char **args) {
+    if (send_request_header(sock_fd, LIST, args[3]) == -1) {
+        return;
+    }
 
-int handle_list(int sock_fd, char **args) {
-    return 0;
+    shutdown(sock_fd, SHUT_WR);
+
+    // Read the response
+    char *err = NULL;
+    if (read_response_code(sock_fd, &err) == -1) {
+        print_invalid_response();
+        return;
+    }
+    if (err != NULL) {
+        print_error_message(err);
+        free(err);
+        return;
+    }
+
+    // Read the size of filenames.
+    size_t size;
+    if (read_size(sock_fd, &size) == -1) {
+        print_invalid_response();
+        return;
+    }
+
+    size_t received_size = copy_file(sock_fd, STDOUT_FILENO, size);
+    if (received_size > size) {
+        print_received_too_much_data();
+    } else if (received_size < size) {
+        print_too_little_data();
+    }
 }
 
 int main(int argc, char **argv) {
@@ -193,29 +318,25 @@ int main(int argc, char **argv) {
     verb command = check_args(args);
 
     int sock_fd = connect_to_server(args[0], args[1]);
-    int return_code = 0;
 
     switch (command) {
     case GET:
-        return_code = handle_get(sock_fd, args);
+        handle_get(sock_fd, args);
         break;
     case PUT:
-        return_code = handle_put(sock_fd, args);
+        handle_put(sock_fd, args);
         break;
     case DELETE:
-        return_code = handle_delete(sock_fd, args);
+        handle_delete(sock_fd, args);
         break;
     case LIST:
-        return_code = handle_list(sock_fd, args);
+        handle_list(sock_fd, args);
         break;
     default:
         break;
     }
 
     close(sock_fd);
-    if (return_code == 0) {
-        print_connection_closed();
-    }
     free(args);
     return 0;
 }
--- nonstop_networking/common.c
+++ nonstop_networking/common.c
@@ -11,9 +11,13 @@ int write_all(int fd, const void *buff, size_t len) {
     while (sent_len < len) {
         int n = write(fd, buff, len - sent_len);
         if (n == -1 && errno != EINTR) {
+            // An error occurred.
             return -1;
         }
-        sent_len += n;
+
+        if (n > 0) {
+            sent_len += n;
+        }
     }
     return 0;
 }
@@ -22,10 +26,14 @@ int read_all(int fd, void *buff, size_t len) {
     size_t read_len = 0;
     while (read_len < len) {
         int n = read(fd, buff, len - read_len);
-        if (n == -1 && errno != EINTR) {
+        if (n == 0 || (n == -1 && errno != EINTR)) {
+            // EOF or error
             return -1;
         }
-        read_len -= n;
+
+        if (n > 0) {
+            read_len += n;
+        }
     }
     return 0;
 }
