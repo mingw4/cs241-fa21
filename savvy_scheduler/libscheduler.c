d0dd22467af75abd92b9db4b65fcbb08dd08ca6f
mingw4 <mingw4@illinois.edu>
2021-12-01 07:44:40 -0600

    lan_first

--- savvy_scheduler/libscheduler.c
+++ savvy_scheduler/libscheduler.c
@@ -12,6 +12,8 @@
 #include <stdlib.h>
 
 #include "print_functions.h"
+#include "includes/callbacks.h"
+#include "includes/vector.h"
 
 /**
  * The struct to hold the information about a given job
@@ -21,9 +23,24 @@ typedef struct _job_info {
 
     /* TODO: Add any other information and bookkeeping you need into this
      * struct. */
+    double t_left_;
+    double t_ini_;
+    double reach_;
+    double initiation_;
+    double place_;
+    double dur_;
 } job_info;
 
-void scheduler_start_up(scheme_t s) {
+//helpers
+double t_pend;
+double t_rspd;
+double period_;
+comparer_t comparision_func;
+size_t n;
+priqueue_t pqueue;
+scheme_t pqueue_scheme;
+
+void scheduler_ini__up(scheme_t s) {
     switch (s) {
     case FCFS:
         comparision_func = comparer_fcfs;
@@ -50,6 +67,10 @@ void scheduler_start_up(scheme_t s) {
     priqueue_init(&pqueue, comparision_func);
     pqueue_scheme = s;
     // Put any additional set up code you may need here
+    t_rspd = 0;
+    n = 0;
+    t_pend = 0;
+    period_ = 0;
 }
 
 static int break_tie(const void *a, const void *b) {
@@ -58,6 +79,14 @@ static int break_tie(const void *a, const void *b) {
 
 int comparer_fcfs(const void *a, const void *b) {
     // TODO: Implement me!
+    job_info* j_i_a = ((job*) a)->metadata;
+    job_info* j_i_b = ((job*) b)->metadata;
+    if (j_i_b->reach_ < j_i_a->reach_) {
+        return 1;
+    }
+    if (j_i_b->reach_ > j_i_a->reach_) {
+        return -1;
+    }
     return 0;
 }
 
@@ -68,22 +97,55 @@ int comparer_ppri(const void *a, const void *b) {
 
 int comparer_pri(const void *a, const void *b) {
     // TODO: Implement me!
-    return 0;
+    job_info* j_i_a = ((job*) a)->metadata;
+    job_info* j_i_b = ((job*) b)->metadata;
+    if (j_i_b->place_ < j_i_a->place_) {
+        return 1;
+    }
+    if (j_i_b->place_ > j_i_a->place_) {
+        return -1;
+    }
+    return break_tie(a, b);
 }
 
 int comparer_psrtf(const void *a, const void *b) {
     // TODO: Implement me!
-    return 0;
+    job_info* j_i_a = ((job*) a)->metadata;
+    job_info* j_i_b = ((job*) b)->metadata;
+    if (j_i_b->t_left_ < j_i_a->t_left_) {
+        return 1;
+    }
+    if (j_i_b->t_left_ > j_i_a->t_left_) {
+        return -1;
+    }
+    return break_tie(a, b);
 }
 
 int comparer_rr(const void *a, const void *b) {
     // TODO: Implement me!
+    job_info* j_i_a = ((job*) a)->metadata;
+    job_info* j_i_b = ((job*) b)->metadata;
+    if (j_i_b->t_ini_ < j_i_a->t_ini_) {
+        return 1;
+    }
+    if (j_i_b->t_ini_ > j_i_a->t_ini_) {
+        return -1;
+    }
+    return break_tie(a, b);
     return 0;
 }
 
 int comparer_sjf(const void *a, const void *b) {
     // TODO: Implement me!
-    return 0;
+    job_info* j_i_a = ((job*) a)->metadata;
+    job_info* j_i_b = ((job*) b)->metadata;
+    if (j_i_b->dur_ < j_i_a->dur_) {
+        return 1;
+    }
+    if (j_i_b->dur_ > j_i_a->dur_) {
+        return -1;
+    }
+    return break_tie(a, b);
 }
 
 // Do not allocate stack space or initialize ctx. These will be overwritten by
@@ -91,15 +153,68 @@ int comparer_sjf(const void *a, const void *b) {
 void scheduler_new_job(job *newjob, int job_number, double time,
                        scheduler_info *sched_data) {
     // TODO: Implement me!
+    job_info *j_i_ = calloc(1,sizeof(job_info));
+    j_i_->id = job_number;
+    j_i_->dur_ = sched_data->running_time;
+    j_i_->t_ini_ = time;
+    j_i_->place_ = sched_data->priority;
+    j_i_->reach_ = time;
+    j_i_->initiation_ = -1;
+    j_i_->t_left_ = sched_data->running_time;
+    newjob->metadata = j_i_;
+    priqueue_offer(&pqueue, newjob);
+    n++;
 }
 
 job *scheduler_quantum_expired(job *job_evicted, double time) {
     // TODO: Implement me!
-    return NULL;
+    job* j_ = priqueue_peek(&pqueue);
+    if (!j_ && !job_evicted) {
+        return NULL;
+    }
+    job_info* j_i_ = j_->metadata;
+    if (0 > j_i_->initiation_) {
+        j_i_->initiation_ = time;
+    }
+    if (j_ && !job_evicted) {
+        j_i_->t_ini_ = time;
+    }
+    if (RR == pqueue_scheme) {
+        priqueue_offer(&pqueue, j_);
+        priqueue_poll(&pqueue);
+        return priqueue_peek(&pqueue);
+    }
+    if (PPRI == pqueue_scheme) {
+        if (j_ == job_evicted) {
+            return job_evicted;
+        } else {
+            return j_;
+        }
+    }
+    if (PSRTF == pqueue_scheme) {
+        if (j_ == job_evicted) {
+            job_info* j_e_i_ = job_evicted->metadata;
+            j_e_i_ ->t_left_ = j_e_i_->t_left_ + j_e_i_->t_ini_ - time;
+            j_e_i_->t_ini_ = time;
+            return job_evicted;
+        } else {
+            job_info* j_e_i_ = job_evicted->metadata;
+            j_e_i_ ->t_left_ = j_e_i_->t_left_ + j_e_i_->t_ini_ - time;
+            j_i_->t_ini_ = time;
+            return j_;
+        }
+    }
+    return job_evicted;
 }
 
 void scheduler_job_finished(job *job_done, double time) {
     // TODO: Implement me!
+    job_info* j_i_ = job_done->metadata;
+    t_pend = t_pend - j_i_->dur_ - j_i_->reach_ + time;
+    t_rspd = t_rspd - j_i_->initiation_ - j_i_->reach_ + time;
+    period_ = period_ + time - j_i_->reach_;
+    free(j_i_);
+    priqueue_poll(&pqueue);
 }
 
 static void print_stats() {
@@ -110,17 +225,17 @@ static void print_stats() {
 
 double scheduler_average_waiting_time() {
     // TODO: Implement me!
-    return 9001;
+    return t_pend/n;
 }
 
 double scheduler_average_turnaround_time() {
     // TODO: Implement me!
-    return 9001;
+    return period_/n;
 }
 
 double scheduler_average_response_time() {
     // TODO: Implement me!
-    return 9001;
+    return t_rspd/n;
 }
 
 void scheduler_show_queue() {
