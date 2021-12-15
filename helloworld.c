679171eb97e3c4d14327a3f164f4c2ad092207f6
mingw4 <mingw4@illinois.edu>
2021-12-15 07:38:58 -0600

    Q2++

--- helloworld.c
+++ helloworld.c
@@ -11,14 +11,14 @@ int main(int argc, char* argv[])
    
     if (strcmp(argv[0], "./encrypt") == 0) {
 	if(argc != 3){
-	     printf("Hello World");
+	     printf("Hello World\n");
 	     return 0;
 	}   
  
         int fp_r = open(argv[1], O_RDWR);
         
         if(fp_r == -1){
-	    printf("Hellow World");
+	    printf("Hellow World\n");
 	    return 0;
 	}
         struct stat statbuf;
@@ -36,15 +36,15 @@ int main(int argc, char* argv[])
        
         FILE* fp_w = fopen(argv[2], "wb+");
 	if(fp_w == NULL){
-	    printf("Hello World");
+	    printf("Hello World\n");
 	    return 0;
 	}
-        for (size_t i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             fputc(rd_data[i], fp_w);
         }
  
         
-        for (size_t i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             int en_byte = rd_data[i]^addr[i];
             fputc(en_byte, fp_w);            
             addr[i] = 0xff;
@@ -58,12 +58,12 @@ int main(int argc, char* argv[])
     }
     else if (strcmp(argv[0], "./decrypt") == 0) {
 	if(argc != 3){
-	    printf("Hello World");
+	    printf("Hello World\n");
 	    return 0;
 	}
 	int fp = open(argv[1], O_RDWR);
 	if(fp == -1){
-	    printf("Hell0 World");
+	    printf("Hell0 World\n");
 	    return 0;
 	}
 
@@ -79,10 +79,10 @@ int main(int argc, char* argv[])
 	
         FILE * fp_w_2 = fopen(argv[2], "wb+");
 	if(fp_w_2 == NULL){
-	    printf("Hello World");
+	    printf("Hello World\n");
 	    return 0;
 	}
-        for (size_t i = 0; i < len/2; i++) {
+        for (int i = 0; i < len/2; i++) {
             int de_byte = data[i]^data[len/2+i];
             fputc(de_byte, fp_w_2);
         }
@@ -91,7 +91,7 @@ int main(int argc, char* argv[])
         fclose(fp_w_2);
 	return 0;
     }else  {
-	printf("Hello World");
+	printf("Hello World\n");
 	return 0;}
 
     return 0;
